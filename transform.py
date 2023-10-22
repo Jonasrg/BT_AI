5d3ec48ccc2a237cb6fe84daf539517a532ef2c9
Jonasrg <101995080+Jonasrg@users.noreply.github.com>
2023-10-22 18:11:44 +0200

    add prep function

--- transform.py
+++ transform.py
@@ -10,4 +10,11 @@ def tf_search_biblio(ls:list)->pd.DataFrame:
     df["document_date_month"] = df["document_date"].dt.month
     # create full document id (publication number)
     df["document_id"] = df["document_country"] + df["document_doc-number"] + df["document_kind"]
-    
+    return df
+
+def prep_eurostat_data(data_path:str, code_path:str)->pd.DataFrame:
+    codes = pd.read_csv(code_path, sep="\t", header=None, names=["indic_sb", "name"])
+    sbs_stats = pd.read_csv(data_path)
+    df =  sbs_stats.merge(codes, on="indic_sb", how="left")
+    df.drop(columns=["STRUCTURE","STRUCTURE_ID", "freq" ], inplace=True)
+    return df
